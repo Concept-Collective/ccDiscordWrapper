1b822a37f1009b7da745179995f0196c1562f325
Mr. K V <36492979+69u@users.noreply.github.com>
2022-12-31 15:43:46 +0800

    update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -15,20 +15,20 @@ server_scripts {
     'env.js',
 }
 
-DiscordRPCEnabled 'true'
-DiscordAppId '832645255286882305'
-DiscordRPCAssetLarge 'ccLogo'
-DiscordRPCTextLarge 'Large Text'
-DiscordRPCAssetSmall 'ccLogo'
-DiscordRPCTextSmall 'Small Text'
-DiscordRPCAction1URL 'https://conceptcollective.net'
-DiscordRPCAction1Text 'Website'
-DiscordRPCAction2URL 'fivem://connect/cfx.re/join/jmxv9m'
-DiscordRPCAction2Text 'Join Now!'
+DiscordRPCEnabled 'true'                                                                                            -- Whether or not to enable CC-DiscordStatus Discord Rich Presence modules
+DiscordAppId '832645255286882305'                                                                                   -- Discord Application ID (can be found at the Discord Developer Portal: https://discord.com/developers/applications)
+DiscordRPCAssetLarge 'ccLogo'                                                                                       -- Discord Rich Presence Large Asset Name (https://discord.com/developers/applications/xxxxxxxxxxxxxxxxxxxx/rich-presence/assets)
+DiscordRPCTextLarge 'Large Text'                                                                                    -- Discord Rich Presence Large Asset Hover Text 
+DiscordRPCAssetSmall 'ccLogo'                                                                                       -- Discord Rich Presence Small Asset Name (https://discord.com/developers/applications/xxxxxxxxxxxxxxxxxxxx/rich-presence/assets)
+DiscordRPCTextSmall 'Small Text'                                                                                    -- Discord Rich Presence Small Asset Hover Text 
+DiscordRPCAction1URL 'https://conceptcollective.net'                                                                -- Discord Rich Presence Action #1 URL
+DiscordRPCAction1Text 'Website'                                                                                     -- Discord Rich Presence Action #1 Text 
+DiscordRPCAction2URL 'fivem://connect/cfx.re/join/jmxv9m'                                                           -- Discord Rich Presence Action #2 URL
+DiscordRPCAction2Text 'Join Now!'                                                                                   -- Discord Rich Presence Action #2 Text 
 
-DiscordStatusEnabled 'true'
-DiscordStatusChannelId '933964742651969556'
-DiscordStatusPlayerNames 'true'
-DiscordStatusEmbedTitle 'Concept Collective'
-DiscordStatusEmbedURL 'https://conceptcollective.net'
-DiscordStatusEmbedThumbnail 'https://cdn.discordapp.com/attachments/672308435534086149/1053879929848209418/idk.png'
+DiscordStatusEnabled 'true'                                                                                         -- Whether or not to enable the CC-DiscordStatus Discord Player Count module
+DiscordStatusChannelId '933964742651969556'                                                                         -- Discord Channel ID to send embed messages to (https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-)
+DiscordStatusPlayerNames 'true'                                                                                     -- True: Will show player names along side players Discord || False: Will only show player count
+DiscordStatusEmbedTitle 'Concept Collective'                                                                        -- Discord message embed Title
+DiscordStatusEmbedURL 'https://conceptcollective.net'                                                               -- Discord message embed URL
+DiscordStatusEmbedThumbnail 'https://cdn.discordapp.com/attachments/672308435534086149/1053879929848209418/idk.png' -- Discord message embed Thumbnail
