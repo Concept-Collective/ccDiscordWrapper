a8be8a25999b08cff18519cb6bb1132685dae2c0
Mr. K V <36492979+69u@users.noreply.github.com>
2023-07-22 19:06:54 +0800

    Update env.example.js

--- env.example.js
+++ env.example.js
@@ -1,4 +1,6 @@
 let sv_config = {};
-sv_config.Discord_Token = ''; /* Discord Application Token Here */
+
+sv_config.Discord_Token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'; // Discord Application Token Here (https://discord.com/developers/applications)
+sv_config.Discord_Webhook  = 'https://discord.com/api/webhooks/xxxxxxxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' // Discord Webhook URL Here (https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks)
 
 module.exports = sv_config;
